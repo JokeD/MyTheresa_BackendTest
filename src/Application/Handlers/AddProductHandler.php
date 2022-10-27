bff6e976b0cfafb331a6bbba490d67c5fde36cfe
jjdiaz <jjdiaz@broadland.group>
2022-10-27 23:36:11 +0200

    Implementing DomainEventDispatcher in AddProductHandler to releaseEvents using command bus and doctrine transaction middleware

--- src/Application/Handlers/AddProductHandler.php
+++ src/Application/Handlers/AddProductHandler.php
@@ -9,25 +9,35 @@ use App\Application\Command\AddProductCommand;
 use App\Domain\Price;
 use App\Domain\Product;
 use App\Domain\ProductRepositoryInterface;
+use App\Domain\Shared\DomainEventDispatcherInterface;
 
 class AddProductHandler
 {
     private ProductRepositoryInterface $productRepository;
+    private DomainEventDispatcherInterface $domainEventDispatcher;
 
-    public function __construct(ProductRepositoryInterface $productRepository)
+    public function __construct(
+        ProductRepositoryInterface $productRepository,
+        DomainEventDispatcherInterface $domainEventDispatcher
+    )
     {
-        $this->productRepository = $productRepository;
+        $this->productRepository     = $productRepository;
+        $this->domainEventDispatcher = $domainEventDispatcher;
     }
 
     public function __invoke(AddProductCommand $addProductCommand): void
     {
-        $this->productRepository->add(
-            new Product(
-                $addProductCommand->sku(),
-                $addProductCommand->category(),
-                $addProductCommand->name(),
-                Price::create($addProductCommand->priceOriginal(), $addProductCommand->priceCurrency()),
-            )
+        $product = new Product(
+            $addProductCommand->sku(),
+            $addProductCommand->category(),
+            $addProductCommand->name(),
+            Price::create(
+                $addProductCommand->priceOriginal(),
+                $addProductCommand->priceCurrency()
+            ),
         );
+        $this->productRepository->add($product);
+        $this->domainEventDispatcher->dispatchAll($product->releaseEvents());
+
     }
 }
