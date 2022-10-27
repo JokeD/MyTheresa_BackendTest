e049686e6706362cab7fafc9d4baff52279da8d6
jjdiaz <jjdiaz@broadland.group>
2022-10-27 23:32:10 +0200

    Changing to DateTime instead of immutable doctrine mapping doesn't recognize (need investigate)

--- src/Domain/ProductWasCreated.php
+++ src/Domain/ProductWasCreated.php
@@ -8,16 +8,16 @@ use App\Domain\Shared\DomainEvent;
 
 class ProductWasCreated implements DomainEvent
 {
-    private \DateTimeImmutable $occurredOn;
+    private \DateTime $occurredOn;
     private Product $product;
 
     public function __construct(Product $product)
     {
-        $this->occurredOn = new \DateTimeImmutable();
+        $this->occurredOn = new \DateTime();
         $this->product    = $product;
     }
 
-    public function occurredOn(): \DateTimeImmutable
+    public function occurredOn(): \DateTime
     {
         return $this->occurredOn;
     }
